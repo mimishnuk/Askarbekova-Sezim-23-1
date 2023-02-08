10448d7eed162825cc235576d2d0b8fd2ca0d84b
mimishnuk <asezim393@gmail.com>
2023-02-08 13:00:13 +0300

    hw1-6

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ from Afisha import settings
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Afisha.settings.base',)
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Afisha.settings',)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
